07f961413aee5825f03b693e187df8736b815f65
Magnus Dewett <Magnusdew@gmail.com>
2018-04-17 13:53:31 +0200

    Init ATM unit

--- TestUnit/UT_ATM.cs
+++ TestUnit/UT_ATM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
 using TransponderLib;
 using TransponderReceiver;
@@ -13,29 +14,51 @@ namespace TestUnit
     [TestFixture]
     class UT_ATM
     {
-        private ATM _uut;
-        private Plane plane;
+        private ATM _atm;
+        private Plane _plane1;
+        private Plane _plane2;
+
+        private ITransponderReceiver _receiver;
+        private ITransponderDataParser _dataParser;
+        private IOutput _output;
+        private ICollisionDetector _detector;
+        private CollisionEventArgs _collisionEventArgs;
+
         [SetUp]
         public void SetUp()
         {
+            _receiver = Substitute.For<ITransponderReceiver>();
+            _dataParser = new TransponderDataParser();
+            _output = Substitute.For<IOutput>();
+            _detector = Substitute.For<ICollisionDetector>();
+
+            _plane1 = new Plane() { Tag = "A3", XCoord = 39045, YCoord = 12932, Altitude = 14000, LastUpdated = _dataParser.ParseTime("20151006213456789") };
+            _plane2 = new Plane() { Tag = "A4", XCoord = 29045, YCoord = 13932, Altitude = 15000, LastUpdated = _dataParser.ParseTime("20151006213456789") };
+
+            _atm = new ATM(_receiver, _dataParser, _output, _detector);
         }
+
+        [Test]
+        public void DetectorOnNoSeperationEvent_ExpectedResult_True()
+        {
+
+            
+        }
+
+        // 
 
         [Test]
 		[Ignore ("Not done")]
         public void wat()
         {
-            plane.Tag = "ARF234";
-            plane.XCoord = 23300;
-            plane.YCoord = 20200;
-            plane.Altitude = 12000;
-            plane.Course = 33.3;
-            plane.Speed = 200.42;
+            _plane1.Course = 33.3;
+            _plane1.Speed = 200.42;
 
             var date = DateTime.Parse("2018-11-22 01:13:37.323");
 
-            _uut.UpdatePlane(plane, 23800, 21000, 11500, date);
+            _atm.UpdatePlane(_plane1, 23800, 21000, 11500, date);
 
-            Assert.That(plane.XCoord, Is.EqualTo(23800));
+            Assert.That(_plane1.XCoord, Is.EqualTo(23800));
         }
     }
 }
