3726b6cbdc94930ad2a6325e19f9967f0f4bb09a
Simon Vu <simon_vu1@hotmail.com>
2018-04-17 11:12:40 +0200

    hihihi

--- ATM.Test.Integration/IT1_SomethingTest.cs
+++ ATM.Test.Integration/IT1_SomethingTest.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using TransponderReceiver;
 
 namespace ATM.Test.Integration
 {
 	[TestFixture]
-	public class IT1_SomethingTest
+	public class IT1_ATMPlaneParser
 	{
+		private ITransponderReceiver _receiver;
+
 		[TestCase]
 		public void TryingToTest()
 		{
